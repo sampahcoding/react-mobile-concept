7885f55f2a718fc8b8e5d220c5d57e7e10b205ff
ana syarifah <anasyarifah@192.168.1.10>
2018-03-21 15:19:35 +0700

    update architecture

--- App.js
+++ App.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
-import AutoHeightImage from 'react-native-auto-height-image';
-import { width } from 'react-native-dimension';
-import styles from './styles/style-landing';
+import MainPage from './src/components/MainPage';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <FlatList
-          data={[{key: 1, image: "https://img-9gag-fun.9cache.com/photo/avOon1W_460swp_v3.webp", title: "Start it now"},
-                {key: 2, image: "https://img-9gag-fun.9cache.com/photo/agX5XXq_460s.jpg", title: "Stop that now"},
-                {key: 3, image: "https://img-9gag-fun.9cache.com/photo/aR30A22_460s.jpg", title: "Don't make a mess in the head"}]}
-          renderItem={({item}) =>
-              <View key={item.key} style={styles.list}>
-                <Text style={styles.heading} >{item.title}</Text>
-                <AutoHeightImage
-                    style={styles.image}
-                    width= {width(100)}
-                    source={{uri: item.image}}
-                />
-              </View>
-          }
-          />
-      </View>
+      <MainPage />
     );
   }
 }

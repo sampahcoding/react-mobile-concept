d7210e38df06b0e0a202dd805b1ff62e653c05d1
ana syarifah <anasyarifah@192.168.1.10>
2018-03-21 14:28:17 +0700

    init and styling structure

---
+++ styles/style-landing.js
@@ -0,0 +1,22 @@
+module.exports = {
+  container: {
+    flex: 1,
+    backgroundColor: '#e8e8e8',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingTop: 40,
+    justifyContent: 'center',
+  },
+  list: {
+    marginBottom: 20,
+    backgroundColor: '#fff'
+  },
+  heading: {
+    alignItems: 'flex-start',
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginTop: 10,
+    marginBottom: 10,
+    paddingLeft: 10
+  }
+};
